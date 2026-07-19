113185345e35cc20321c3405fe40a22cf0e23df6
menyawi356 <mhatm9387@gmail.com>
2026-07-20 00:25:00 +0300

    Update GetCertificate.jsx

--- src/pages/GetCertificate.jsx
+++ src/pages/GetCertificate.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
 const GetCertificate = () => {
+  const containerStyle = {
+    width: '100%',
+    height: '950px',
+    border: 'none',
+    display: 'block'
+  };
+
   return (
-    <div 
-      style={{
-        width: '100%',
-        height: '950px',
-        border: 'none',
-        display: 'block'
-      }}
-    >
-      {/* Add your certificate content here */}
-      <h1>Get Certificate</h1>
-      <p>Your certificate content goes here</p>
+    <div style={containerStyle}>
+      {/* Your content here */}
     </div>
   );
 };
