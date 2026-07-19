126686985c5ce810d01543aa9ce84a42c9ce2f6f
menyawi356 <mhatm9387@gmail.com>
2026-07-19 19:07:16 +0300

    Update GetCertificate.jsx

--- src/pages/GetCertificate.jsx
+++ src/pages/GetCertificate.jsx
@@ -1,13 +1,6 @@
-<iframe
-  src="/certificate.html"
-  title="Get Your Certificate"
-  className="certificate-frame"
-  loading="lazy"
-  style={{
-    width: '100%',
-    height: '950px',
-    minHeight: '800px',
-    border: 'none',
-    display: 'block'
-  }}
-/>
+style={{
+  width: '100%',
+  height: '950px',
+  border: 'none',
+  display: 'block'
+}}
