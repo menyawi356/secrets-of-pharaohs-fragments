079495b1388f82f82df9ea7b47003a296b35eff6
menyawi356 <mhatm9387@gmail.com>
2026-07-20 00:27:27 +0300

    Update GetCertificate.jsx

--- src/pages/GetCertificate.jsx
+++ src/pages/GetCertificate.jsx
@@ -10,7 +10,22 @@ const GetCertificate = () => {
 
   return (
     <div style={containerStyle}>
-      {/* Your content here */}
+      {/* Add your certificate content here */}
+      <h1>Get Your Certificate</h1>
+      
+      {/* If you had an iframe or embedded content, add it back */}
+      <iframe
+        src="YOUR_CERTIFICATE_URL_HERE" // Put your actual URL here
+        style={{
+          width: '100%',
+          height: '900px',
+          border: 'none'
+        }}
+        title="Certificate Viewer"
+      />
+      
+      {/* Or if you had other content, add it back */}
+      <p>Complete your training to receive your certificate</p>
     </div>
   );
 };
