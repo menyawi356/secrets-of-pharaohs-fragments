2f4ce6420edfe8d467d49bfff070b0d90a645d79
menyawi356 <mhatm9387@gmail.com>
2026-07-20 19:40:45 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Optional: Add these for better compatibility
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'dist',
-  }
 })
